afd78d43dcd63ad79be9c2729a1b98b98956fa72
Annayingyang <2540640@students.wits.ac.za>
2025-03-23 15:36:34 +0200

    i hope this doesnt break the game

---
+++ LMP Game Assignment 1/Assets/Scripts/MenuScript.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuScript : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        SceneManager.LoadScene("Main");
+    }
+
+    public void Credits()
+    {
+        SceneManager.LoadScene("Credits");
+    }
+
+    public void CloseScene()
+    {
+        SceneManager.LoadScene("Menu");
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
