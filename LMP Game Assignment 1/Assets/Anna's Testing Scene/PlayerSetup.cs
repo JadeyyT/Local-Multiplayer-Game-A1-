d0dca9f87e5cec2b86664404776d2e0d8c6c11ff
Annayingyang <2540640@students.wits.ac.za>
2025-03-24 02:24:19 +0200

    controller input system
    Bismillahir Rahmanir Raheem
    Ya Allah SWT please make this work.
    Ameen

---
+++ LMP Game Assignment 1/Assets/Anna's Testing Scene/PlayerSetup.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerSetup : MonoBehaviour
+{
+    public ControllerInput player1ControllerInput;
+    public ControllerInput player2ControllerInput;
+
+    void Start()
+    {
+        
+        var devices = InputSystem.devices;
+
+        
+        if (devices.Count > 0 && devices[0] is Gamepad)
+        {
+            player1ControllerInput.playerDevice = devices[0];
+            Debug.Log("Player 1 assigned to device: " + devices[0].name);
+        }
+
+        
+        if (devices.Count > 1 && devices[1] is Gamepad)
+        {
+            player2ControllerInput.playerDevice = devices[1];
+            Debug.Log("Player 2 assigned to device: " + devices[1].name);
+        }
+    }
+}
